c157dda72ea0498f32631332bd42cbc5a5e93da7
snowflakelearning88@gmail.com <snowflakelearning88@gmail.com>
2022-07-11 10:51:32 +0000

    updated commit on jinja excercise

--- models/example/read_1_to_10_using_jinja.sql
+++ models/example/read_1_to_10_using_jinja.sql
@@ -1,5 +1,14 @@
+ {{
+     config(
+         materialized='table'
+     )
+ }}
+ with sourcedata as
+ (
  {% for i in range(10) %}
 select {{i }} as number {% if not loop.last %} union all
     
 {% endif %}
 {% endfor %}
+ )
+ select * from sourcedata;
